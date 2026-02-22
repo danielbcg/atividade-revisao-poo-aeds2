e232e0c73c93f7873f527cfb928b509cc9436f02
DanielBonypucminas <danielbony2105@gmail.com>
2026-02-22 09:44:47 -0300

    feat: implementação de atributos privados em ProdutoPerecivel

---
+++ src/ProdutoPerecivel.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class ProdutoPerecivel {
+    private static final double DESCONTO = 0.25;
+    private static final int PRAZO = 7;
+    private LocalDate dataDeValidade;
+    
+    
+}
